00cb6d8071e4f82d99cdc09bd4936cce932a4ab5
Oslier Oliveira <88196603+OslierOliveira@users.noreply.github.com>
2021-08-24 22:14:55 -0300

    acrescentando mais campos no relatorio - 02

--- js/main.js
+++ js/main.js
@@ -13,19 +13,25 @@ function criaLinha(queue) {
     tdbirth = document.createElement("td");
     tdname = document.createElement("td");
     tdgender = document.createElement("td");
-    tdemail = document.createElement("td");
+    tdemail = document.createElement("td");    
+    tdphone = document.createElement("td"); 
+    tdaddress = document.createElement("td");   
 
     tdid.innerHTML = queue.id
     tdbirth.innerHTML = queue.birth
     tdname.innerHTML = queue.name
     tdgender.innerHTML = queue.gender
     tdemail.innerHTML = queue.email
+    tdphone.innerHTML = queue.phone
+    tdaddress.innerHTML = queue.address
 
     linha.appendChild(tdid);
     linha.appendChild(tdbirth);
     linha.appendChild(tdname);
     linha.appendChild(tdgender);
     linha.appendChild(tdemail);
+    linha.appendChild(tdphone);
+    linha.appendChild(tdaddress);
 
 
     return linha;
